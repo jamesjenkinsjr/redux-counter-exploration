ceca334b56a43d7382c6941cf7bec0a6aa90363f
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-02 18:36:17 -0500

    Create redux store, counter reducer, implement increment/decrement - no rerender

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
+
+//initial state
+const initialState = 0;
+//reducer (how state changes)
+const countReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+//creation of the store
+const store = createStore(
+  countReducer, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+//temp. actions
+const increment = {type: "INCREMENT"};
+const decrement = {type: "DECREMENT"};
+
+//dispatch
+store.dispatch(increment);
+store.dispatch(increment);
+store.dispatch(decrement);
 
 class App extends Component {
   render() {
     return (
       <div>
-        SWAG
+        <h1>SWAG</h1>
+        <button onClick={() => store.dispatch({type: "INCREMENT"})}>+</button>
+        <button onClick={() => store.dispatch({type: "DECREMENT"})}>-</button>
+        <h1>{store.getState()}</h1>
       </div>
     );
   }
